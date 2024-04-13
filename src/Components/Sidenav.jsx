1fde6afb02759b46ba05575e6fffebb89e561032
Harsh <harshkumar9528@gmail.com>
2024-04-13 16:34:29 +0530

    final-commit

--- src/Components/Sidenav.jsx
+++ src/Components/Sidenav.jsx
@@ -31,7 +31,7 @@ nav ? (
  </a>
  <a onClick={handleNav} href="#work" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-400 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
  <GrProjects size={20} />
-  <span className='pl-4'>Work</span>
+  <span className='pl-4'>Education</span>
   </a>
  <a onClick={handleNav} href="#projects" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-400 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
 <AiOutlineProject size={20} />
